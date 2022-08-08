92cca826e85a211c0dde24c4fcedd43e4bd106aa
Ahmad Fauzan <ahmad.fauzan@swiftsolutions.id>
2022-08-08 23:27:06 +0700

    result output json

--- go.mod
+++ go.mod
@@ -3,11 +3,10 @@ module tasking-rest-api
 go 1.14
 
 require (
-	github.com/gin-gonic/gin v1.8.1 // indirect
+	github.com/gin-gonic/gin v1.8.1
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
 	github.com/google/uuid v1.3.0
-	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"tasking-rest-api/task"
 
+	_ "github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// dsn := "root:root@tcp(127.0.0.1:3306)/tasking_app?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connection to database is good.")
+
+	// var tasks []task.Task
+
+	// db.Find(&tasks)
+
+	// length := len(tasks)
+
+	// fmt.Println(length)
+
+	// for _, task := range tasks {
+	// 	fmt.Println(task.Assign)
+	// 	fmt.Println(task.Deadline)
+	// }
+
+	router := gin.Default()
+	router.GET("handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:root@tcp(127.0.0.1:3306)/tasking_app?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -17,18 +46,9 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database is good.")
-
 	var tasks []task.Task
 
 	db.Find(&tasks)
 
-	length := len(tasks)
-
-	fmt.Println(length)
-
-	for _, task := range tasks {
-		fmt.Println(task.Assign)
-		fmt.Println(task.Deadline)
-	}
+	c.JSON(http.StatusOK, tasks)
 }
