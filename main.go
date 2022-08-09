6e9e6874bdf93fbe1d10ed6107d0a63e13f3d1b6
ahmad fauzan <fauzan.c4@gmail.com>
2022-08-09 20:16:21 +0700

    task service

--- main.go
+++ main.go
@@ -19,13 +19,21 @@ func main() {
 	}
 
 	taskRepository := task.NewRepository(db)
-	id := uuid.Must(uuid.MustParse("a15601a8-e41d-4281-969a-f5c991c44346"), err)
-	tasks, err := taskRepository.FindByTaskID(id)
+
+	taskService := task.NewService(taskRepository)
+
+	var id uuid.UUID = uuid.MustParse("a15601a8-e41d-4281-969a-f5c991c44346")
+	tasks, _ := taskService.FindTasks(id)
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 	fmt.Println("debug")
-	for _, task := range tasks {
-		fmt.Println(task.Assign)
-	}
+	fmt.Println(len(tasks))
+	// for _, task := range tasks {
+	// 	fmt.Println(task.Assign)
+	// }
 
 	// router := gin.Default()
 	// api := router.Group("/api/v1")
--- task/service.go
+++ task/service.go
@@ -1,1 +1,33 @@
 package task
+
+import "github.com/google/uuid"
+
+type Service interface {
+	FindTasks(Id uuid.UUID) ([]Task, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) FindTasks(Id uuid.UUID) ([]Task, error) {
+	if Id != uuid.Nil {
+		tasks, err := s.repository.FindByTaskID(Id)
+		if err != nil {
+			return tasks, err
+		}
+
+		return tasks, nil
+	}
+
+	tasks, err := s.repository.FindAll()
+	if err != nil {
+		return tasks, err
+	}
+
+	return tasks, nil
+}
